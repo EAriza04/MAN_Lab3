759ecb477892cb5b8974aaf417fb66a585d23d0c
Enri238 <enrihomipat@icloud.com>
2025-04-05 19:12:06 +0200

    Valjent

--- src/test/java/org/mps/ronqi2/ronQI2Silvertest.java
+++ src/test/java/org/mps/ronqi2/ronQI2Silvertest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mps.dispositivo.DispositivoSilver;
 
-public class ronQI2Silvertest {
+public class RonQI2SilverTest {
 
 
     /*
@@ -64,15 +64,67 @@ public class ronQI2Silvertest {
      * Un reconectar, comprueba si el dispositivo desconectado, en ese caso, conecta ambos y devuelve true si ambos han sido conectados. 
      * Genera las pruebas que estimes oportunas para comprobar su correcto funcionamiento. 
      * Centrate en probar si todo va bien, o si no, y si se llama a los métodos que deben ser llamados.
-    */
+     */
+
+    @Test
+    @DisplayName("Reconectar Dispositivo Correctamente Devuelve True")
+    void Reconectar_DispositivoCorrecto_DevuelveTrue(){
+        // Arrange
+        DispositivoSilver ds = mock(DispositivoSilver.class);
+        when(ds.estaConectado()).thenReturn(false);
+        when(ds.conectarSensorPresion()).thenReturn(true);
+        when(ds.conectarSensorSonido()).thenReturn(true);
+        RonQI2Silver rq2s = new RonQI2Silver();
+        rq2s.anyadirDispositivo(ds);
+
+        // Act + Assert
+        assertTrue(rq2s.reconectar());
+        verify(ds, times(1)).conectarSensorPresion();
+        verify(ds, times(1)).conectarSensorSonido();
+    }
+
+    @Test
+    @DisplayName("Reconectar Dispositivo Incorrectamente Devuelve False")
+    void Reconectar_DispositivoIncorrecto_DevuelveFalse(){
+        // Arrange
+        DispositivoSilver ds = mock(DispositivoSilver.class);
+        when(ds.estaConectado()).thenReturn(false);
+        when(ds.conectarSensorPresion()).thenReturn(false);
+        when(ds.conectarSensorSonido()).thenReturn(false);
+        RonQI2Silver rq2s = new RonQI2Silver();
+        rq2s.anyadirDispositivo(ds);
+
+        // Act + Assert
+        assertFalse(rq2s.reconectar());
+        verify(ds, times(1)).conectarSensorPresion();
+    }
     
     /*
      * El método evaluarApneaSuenyo, evalua las últimas 5 lecturas realizadas con obtenerNuevaLectura(), 
      * y si ambos sensores superan o son iguales a sus umbrales, que son thresholdP = 20.0f y thresholdS = 30.0f;, 
      * se considera que hay una apnea en proceso. Si hay menos de 5 lecturas también debería realizar la media.
-    */
+     */
+     
+    @Test
+    @DisplayName("Evaluar Apnea Suenyo Correctamente Devuelve True")
+    void EvaluarApneaSuenyo_Correctamente_DevuelveTrue(){
+        // Arrange
+        DispositivoSilver ds = mock(DispositivoSilver.class);
+        RonQI2Silver rq2s = new RonQI2Silver();
+        rq2s.anyadirDispositivo(ds);
+        when(ds.leerSensorPresion()).thenReturn(25.0f);
+        when(ds.leerSensorSonido()).thenReturn(35.0f);
 
-    /* Realiza un primer test para ver que funciona bien independientemente del número de lecturas.
+        // Act
+        for (int i = 0; i < 5; i++) {
+            rq2s.obtenerNuevaLectura();
+        }
+        
+        // Assert
+        assertTrue(rq2s.evaluarApneaSuenyo());
+    }
+
+     /* Realiza un primer test para ver que funciona bien independientemente del número de lecturas.
      * Usa el ParameterizedTest para realizar un número de lecturas previas a calcular si hay apnea o no (por ejemplo 4, 5 y 10 lecturas).
      * https://junit.org/junit5/docs/current/user-guide/index.html#writing-tests-parameterized-tests
     */
